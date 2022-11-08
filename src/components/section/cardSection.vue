abdbbbd1005a86d76ea30b4fd357c473a979e718
hmm365 <happyham52@gmail.com>
2022-11-08 16:21:24 +0900

    1108

--- src/components/section/cardSection.vue
+++ src/components/section/cardSection.vue
@@ -1,162 +1,144 @@
 <template>
-	<section id="cardType" class="parallax card__wrap" :class="attr">
-		<h2>밤하늘 과 관련된 시</h2>
-		<p>밤하늘 또는 야천(夜天)은 밤에 보이는 하늘을 이르는 말입니다.</p>
-		<div class="card__inner" :class="layout">
-			<article class="card">
-				<figure class="card__header">
-					<img src="../../assets/img/card_bg01.jpg" alt="고양이 사진1" />
-				</figure>
-				<div class="card__body">
-					<h3 class="tit">저녁 에</h3>
-					<p class="desc">
-						저렇게 많은 별 중에서<br />
-						별 하나가 나를 내려다본다<br />
-						이렇게 많은 사람 중에서<br />
-						그 별 하나를 쳐다본다
-					</p>
-					<a class="btn" href="/">
-						더 자세히 보기
-						<span aria-hidden="true">
-							<svg width="81" height="8" viewBox="0 0 81 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path
-									d="M80.3536 4.35355C80.5488 4.15829 80.5488 3.84171 80.3536 3.64645L77.1716 0.464466C76.9763 0.269204 76.6597 0.269204 76.4645 0.464466C76.2692 0.659728 76.2692 0.976311 76.4645 1.17157L79.2929 4L76.4645 6.82843C76.2692 7.02369 76.2692 7.34027 76.4645 7.53553C76.6597 7.7308 76.9763 7.7308 77.1716 7.53553L80.3536 4.35355ZM0 4.5H80V3.5H0V4.5Z"
-									fill="#B5B5B5"
-								/>
-							</svg>
-						</span>
-					</a>
-				</div>
-			</article>
-			<article class="card">
-				<figure class="card__header">
-					<img src="../../assets/img/card_bg02.jpg" alt="고양이 사진2" />
-				</figure>
-				<div class="card__body">
-					<h3 class="tit">어떤 마을</h3>
-					<p class="desc">
-						사람들이 착하게 사는지 별들이 많이 떴다<br />
-						개울물 맑게 흐르는 곳에 마을을 이루고<br />
-						물바가지에 떠담던 접동새소리 별 그림자<br />
-						그 물로 쌀을 씻어 밥짓는 냄새 나면<br />
-						굴뚝 가까이 내려오던<br />
-						밥티처럼 따스한 별들이 뜬 마을을 지난다<br />
-						사람들이 순하게 사는지 별들이 참 많이 떴다<br />
-					</p>
-					<a class="btn" href="/">
-						더 자세히 보기
-						<span aria-hidden="true">
-							<svg width="81" height="8" viewBox="0 0 81 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path
-									d="M80.3536 4.35355C80.5488 4.15829 80.5488 3.84171 80.3536 3.64645L77.1716 0.464466C76.9763 0.269204 76.6597 0.269204 76.4645 0.464466C76.2692 0.659728 76.2692 0.976311 76.4645 1.17157L79.2929 4L76.4645 6.82843C76.2692 7.02369 76.2692 7.34027 76.4645 7.53553C76.6597 7.7308 76.9763 7.7308 77.1716 7.53553L80.3536 4.35355ZM0 4.5H80V3.5H0V4.5Z"
-									fill="#B5B5B5"
-								/>
-							</svg>
-						</span>
-					</a>
-				</div>
-			</article>
-			<article class="card">
-				<figure class="card__header">
-					<img src="../../assets/img/card_bg03.jpg" alt="고양이 사진3" />
-				</figure>
-				<div class="card__body">
-					<h3 class="tit">별</h3>
-					<p class="desc">
-						어느날 거리엘 나갔다 비를 만나 지나치던 한 처마 아래 들어섰으려니 내 곁에도 역시 나와 한 가지로 멀구러미 하늘을 쳐다보고 비를 긋고 섰는 사나이가 있어, 그의 모습을 보아하니 문득 그 별이 생각났다. 밤마다 뜨락에 내려 우러러 보노라면 만천의 별들 가운데서도 가장 나의 별 가차이
-						나도 모를, 항상 그늘 많은 별 하나-. 영원히 건널 수 없는 심연에 나누어져 말없이 서로 바라보고 지낼 수 밖에 없는 먼 먼 그 별, 그리고 나의 별!
-					</p>
-					<a class="btn" href="/">
-						더 자세히 보기
-						<span>
-							<svg width="81" height="8" viewBox="0 0 81 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path
-									d="M80.3536 4.35355C80.5488 4.15829 80.5488 3.84171 80.3536 3.64645L77.1716 0.464466C76.9763 0.269204 76.6597 0.269204 76.4645 0.464466C76.2692 0.659728 76.2692 0.976311 76.4645 1.17157L79.2929 4L76.4645 6.82843C76.2692 7.02369 76.2692 7.34027 76.4645 7.53553C76.6597 7.7308 76.9763 7.7308 77.1716 7.53553L80.3536 4.35355ZM0 4.5H80V3.5H0V4.5Z"
-									fill="B5B5B5"
-								/>
-							</svg>
-						</span>
-					</a>
-				</div>
-			</article>
-		</div>
-	</section>
+    <section id="cardType" class="parallax card__wrap" :class="attr">
+        <h2>{{ cardTitle.title }}</h2>
+        <p>{{ cardTitle.desc }}</p>
+        <div class="card__inner" :class="layout">
+            <article v-for="cardTexts in cardText" :key="cardTexts" class="card">
+                <figure class="card__header">
+                    <img :src="cardTexts.imgSrc" :alt="cardTexts.imgName" />
+                </figure>
+                <div class="card__body">
+                    <h3 class="tit">{{ cardTexts.cardTitle }}</h3>
+                    <p class="desc">
+                        {{ cardTexts.cardDesc }}
+                    </p>
+                    <a class="btn" :href="cardTexts.btnLink">
+                        더 자세히 보기
+                        <span aria-hidden="true">
+                            <svg width="81" height="8" viewBox="0 0 81 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path :d="cardTexts.cardIcon" :fill="cardTexts.cardIconFill" />
+                            </svg>
+                        </span>
+                    </a>
+                </div>
+            </article>
+        </div>
+    </section>
 </template>
 <script>
 export default {
-	props: {
-		attr: String,
-		layout: String,
-	},
+    props: {
+        attr: String,
+        layout: String,
+    },
+    data() {
+        return {
+            cardTitle: { title: '밤하늘 과 관련된 시', desc: '밤하늘 또는 야천(夜天)은 밤에 보이는 하늘을 이르는 말입니다.' },
+            cardText: [
+                {
+                    imgSrc: 'assets/img/card_bg01.jpg',
+                    imgName: '사진1',
+                    cardTitle: '저녁 에',
+                    cardDesc: '저렇게 많은 별 중에서 \n별 하나가 나를 내려다본다\n이렇게 많은 사람 중에서\n그 별 하나를 쳐다본다',
+                    btnLink: '/',
+                    cardIcon:
+                        'M80.3536 4.35355C80.5488 4.15829 80.5488 3.84171 80.3536 3.64645L77.1716 0.464466C76.9763 0.269204 76.6597 0.269204 76.4645 0.464466C76.2692 0.659728 76.2692 0.976311 76.4645 1.17157L79.2929 4L76.4645 6.82843C76.2692 7.02369 76.2692 7.34027 76.4645 7.53553C76.6597 7.7308 76.9763 7.7308 77.1716 7.53553L80.3536 4.35355ZM0 4.5H80V3.5H0V4.5Z',
+                    cardIconFill: '#B5B5B5',
+                },
+                {
+                    imgSrc: 'assets/img/card_bg02.jpg',
+                    imgName: '사진2',
+                    cardTitle: '어떤 마을',
+                    cardDesc:
+                        '​​저렇게 많은 중에서\n별 하나가 나를 내려다본다.\n이렇게 많은 사람 중에서\n그 별 하나를 쳐다본다.\n​​밤이 깊을수록 별은\n밝음 속에 사라지고\n나는 어둠 속에 사라진다.\n​​이렇게 정다운\n너 하나 나 하나는\n어디서 무엇이 되어 다시 만나랴.',
+                    btnLink: '/',
+                    cardIcon:
+                        'M80.3536 4.35355C80.5488 4.15829 80.5488 3.84171 80.3536 3.64645L77.1716 0.464466C76.9763 0.269204 76.6597 0.269204 76.4645 0.464466C76.2692 0.659728 76.2692 0.976311 76.4645 1.17157L79.2929 4L76.4645 6.82843C76.2692 7.02369 76.2692 7.34027 76.4645 7.53553C76.6597 7.7308 76.9763 7.7308 77.1716 7.53553L80.3536 4.35355ZM0 4.5H80V3.5H0V4.5Z',
+                    cardIconFill: '#B5B5B5',
+                },
+                {
+                    imgSrc: 'assets/img/card_bg03.jpg',
+                    imgName: '사진3',
+                    cardTitle: '별',
+                    cardDesc:
+                        '어느날 거리엘 나갔다 비를 만나 지나치던 한 처마 아래 들어섰으려니 내 곁에도 역시 나와 한 가지로 멀구러미 하늘을 쳐다보고비를 긋고 섰는 사나이가 있어, 그의 모습을 보아하니 문득 그 별이 생각났다. 밤마다 뜨락에 내려 우러러 보노라면 만천의 별들가운데서도 가장 나의 별 가차이 나도 모를, 항상 그늘 많은 별 하나-. 영원히 건널 수 없는 심연에 나누어져 말없이 서로바라보고 지낼 수 밖에 없는 먼 먼 그 별, 그리고 나의 별!',
+                    btnLink: '/',
+                    cardIcon:
+                        'M80.3536 4.35355C80.5488 4.15829 80.5488 3.84171 80.3536 3.64645L77.1716 0.464466C76.9763 0.269204 76.6597 0.269204 76.4645 0.464466C76.2692 0.659728 76.2692 0.976311 76.4645 1.17157L79.2929 4L76.4645 6.82843C76.2692 7.02369 76.2692 7.34027 76.4645 7.53553C76.6597 7.7308 76.9763 7.7308 77.1716 7.53553L80.3536 4.35355ZM0 4.5H80V3.5H0V4.5Z',
+                    cardIconFill: '#B5B5B5',
+                },
+            ],
+        };
+    },
 };
 </script>
 <style>
 /* cardType */
 .card__inner {
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: space-between;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
 }
 .card {
-	width: 32%;
-	background-color: rgba(71, 47, 193, 0.3);
-	border-radius: 23px 23px 20px 20px;
+    width: 32%;
+    background-color: rgba(71, 47, 193, 0.3);
+    border-radius: 23px 23px 20px 20px;
 }
 
 .card__header img {
-	border-radius: 20px 20px 0 0;
+    border-radius: 20px 20px 0 0;
 }
 .card__body {
-	padding: 24px;
+    padding: 24px;
 }
 .card__body .tit {
-	font-size: 24px;
-	margin-bottom: 10px;
-	overflow: hidden;
-	white-space: nowrap;
-	text-overflow: ellipsis;
-	color: #dadada;
+    font-size: 24px;
+    margin-bottom: 10px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    color: #dadada;
 }
 .card__body .desc {
-	font-size: 18px;
-	line-height: 1.4;
-	color: #b5b5b5;
-	margin-bottom: 20px;
-	font-weight: 300;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	display: -webkit-box;
-	-webkit-line-clamp: 3;
-	-webkit-box-orient: vertical;
+    font-size: 18px;
+    line-height: 1.4;
+    color: #b5b5b5;
+    margin-bottom: 20px;
+    font-weight: 300;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
 }
 .card__body .btn {
-	color: #b5b5b5;
-	font-size: 16px;
-	margin-bottom: 24px;
+    color: #b5b5b5;
+    font-size: 16px;
+    margin-bottom: 24px;
 }
 @media (max-width: 960px) {
-	.card {
-		width: 49%;
-	}
-	.card:last-child {
-		display: none;
-	}
+    .card {
+        width: 49%;
+    }
+    .card:last-child {
+        display: none;
+    }
 }
 @media (max-width: 600px) {
-	.card {
-		width: 100%;
-	}
-	.card:first-child {
-		margin-bottom: 20px;
-	}
-	.card__body {
-		padding: 20px;
-	}
-	.card__body .tit {
-		font-size: 18px;
-	}
-	.card__body .desc {
-		font-size: 16px;
-	}
+    .card {
+        width: 100%;
+    }
+    .card:first-child {
+        margin-bottom: 20px;
+    }
+    .card__body {
+        padding: 20px;
+    }
+    .card__body .tit {
+        font-size: 18px;
+    }
+    .card__body .desc {
+        font-size: 16px;
+    }
 }
 </style>
--- src/components/section/imageSection.vue
+++ src/components/section/imageSection.vue
@@ -1,109 +1,123 @@
 <template>
-	<section id="imgType" class="parallax img__wrap" :class="attr">
-		<h2>밤 풍경</h2>
-		<p>밤의 별빛들을 보면 차분해 지는 느낌이 들어요.</p>
-		<div class="image__inner" :class="layout">
-			<article class="image img1">
-				<div class="image__box">
-					<h3 class="image__tit">밤 하늘</h3>
-					<p class="image__desc">아름다운 밤 풍경을 보며 차분한 차 한잔을 하는것도 좋을것 같습니다. 혹은 명상을 하는게 어떠신가요?</p>
-					<a class="image__btn" href="/">자세히보기</a>
-				</div>
-			</article>
-			<article class="image img2">
-				<div class="image__box">
-					<h3 class="image__tit">오로라</h3>
-					<p class="image__desc">주로 지구의 북극권, 남극권 지역에서 관측할 수 있는 천문현상. 초고층 대기 중에 형형색색의 발광(發光)이 나타나며, 이 때문에 번역할 경우 '극광'이라고도 부릅니다.</p>
-					<a class="image__btn brown" href="/">자세히보기</a>
-				</div>
-			</article>
-		</div>
-	</section>
+    <section id="imgType" class="parallax img__wrap" :class="attr">
+        <h2>밤 풍경</h2>
+        <p>밤의 별빛들을 보면 차분해 지는 느낌이 들어요.</p>
+        <div class="image__inner" :class="layout">
+            <article class="image" :class="text.imgClass" v-for="text in imageText" :key="text">
+                <div class="image__box">
+                    <h3 class="image__tit">{{ text.imgTit }}</h3>
+                    <p class="image__desc">{{ text.imgDesc }}</p>
+                    <a class="image__btn" :class="text.btnClass" :href="text.btnLink">자세히보기</a>
+                </div>
+            </article>
+        </div>
+    </section>
 </template>
 <script>
 export default {
-	props: {
-		attr: String,
-		layout: String,
-	},
+    props: {
+        attr: String,
+        layout: String,
+    },
+    data() {
+        return {
+            imageText: [
+                {
+                    imgTit: '밤 하늘',
+                    imgDesc: '아름다운 밤 풍경을 보며 차분한 차 한잔을 하는것도 좋을것 같습니다. 혹은 명상을 하는게 어떠신가요?',
+                    imgClass: 'img1',
+                    btnClass: 'basic',
+                    btnLink: '/',
+                },
+                {
+                    imgTit: '오로라',
+                    imgDesc:
+                        '주로 지구의 북극권, 남극권 지역에서 관측할 수 있는 천문현상. 초고층 대기 중에 형형색색의 발광(發光)이 나타나며, 이 때문에번역할 경우 극광이라고도 부릅니다.',
+                    imgClass: 'img2',
+                    btnClass: 'brown',
+                    btnLink: '/',
+                },
+            ],
+        };
+    },
 };
 </script>
 <style>
 /* imgType */
 .image__inner {
-	display: flex;
-	justify-content: space-between;
+    display: flex;
+    justify-content: space-between;
 }
 .image {
-	width: 49%;
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-end;
-	border-radius: 20px;
-	padding: 210px 20px 20px 20px;
-	box-sizing: border-box;
+    width: 49%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    border-radius: 20px;
+    padding: 210px 20px 20px 20px;
+    box-sizing: border-box;
 }
 .img1 {
-	background-image: url(../../assets/img/img_bg01_01.jpg);
-	background-size: cover;
+    background-image: url(../../assets/img/img_bg01_01.jpg);
+    background-size: cover;
 }
 .img2 {
-	background-image: url(../../assets/img/img_bg01_02.jpg);
-	background-size: cover;
+    background-image: url(../../assets/img/img_bg01_02.jpg);
+    background-size: cover;
 }
 .image__box {
-	margin: 30px;
+    margin: 30px;
 }
 .image__tit {
-	font-size: 32px;
-	color: #dadada;
-	margin-bottom: 10px;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	white-space: nowrap;
+    font-size: 32px;
+    color: #dadada;
+    margin-bottom: 10px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
 }
 .image__desc {
-	font-size: 16px;
-	margin-bottom: 10px;
-	line-height: 1.5;
-	color: #b5b5b5;
-	background-color: rgba(0, 0, 0, 0.08);
-	backdrop-filter: blur(2px);
-	overflow: hidden;
-	text-overflow: ellipsis;
-	display: -webkit-box;
-	-webkit-line-clamp: 2;
-	-webkit-box-orient: vertical;
+    font-size: 16px;
+    margin-bottom: 10px;
+    line-height: 1.5;
+    color: #b5b5b5;
+    background-color: rgba(0, 0, 0, 0.08);
+    backdrop-filter: blur(2px);
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
 }
 .image__btn {
-	display: inline-block;
-	font-size: 16px;
-	padding: 10px 32px 8px 32px;
-	background-color: #646aff;
-	color: #030a1a;
-	border-radius: 15px;
+    display: inline-block;
+    font-size: 16px;
+    padding: 10px 32px 8px 32px;
+    background-color: #646aff;
+    color: #030a1a;
+    border-radius: 15px;
 }
 .image__btn.brown {
-	background-color: #7c5df5;
+    background-color: #7c5df5;
 }
 @media (max-width: 960px) {
 }
 @media (max-width: 600px) {
-	.image__inner {
-		flex-direction: column;
-	}
-	.image {
-		width: 100%;
-		padding: 210px 20px 20px 20px;
-	}
-	.image:first-child {
-		margin-bottom: 20px;
-	}
-	.image__tit {
-		font-size: 24px;
-	}
-	.image__desc {
-		font-size: 16px;
-	}
+    .image__inner {
+        flex-direction: column;
+    }
+    .image {
+        width: 100%;
+        padding: 210px 20px 20px 20px;
+    }
+    .image:first-child {
+        margin-bottom: 20px;
+    }
+    .image__tit {
+        font-size: 24px;
+    }
+    .image__desc {
+        font-size: 16px;
+    }
 }
 </style>
